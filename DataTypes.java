68932759e34e71762300a41712f87ae82ad15dfa
Huey Tieu <huey.tieu@mrjaffesclass.com>
2022-09-15 11:41:44 -0700

    learned boolean things

---
+++ DataTypes.java
@@ -0,0 +1,12 @@
+
+
+public class DataTypes
+{
+    public static void main(String[] args){
+        System.out.println("Hello World!");
+            int i ;
+            double f = 2.5;
+            boolean flag = true;
+            
+    }
+}
