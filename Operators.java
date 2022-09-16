451e955b8f53c6a70d8e3e16de9faf25893c7cd8
Huey Tieu <huey.tieu@mrjaffesclass.com>
2022-09-16 11:41:44 -0700

    i wrote 2 lines of code today

--- Operators.java
+++ Operators.java
@@ -1,12 +1,16 @@
 
 
-public class DataTypes
+public class Operators
 {
     public static void main(String[] args){
         System.out.println("Hello World!");
-            int i ;
+            int i;
+            i = 10;
             double f = 2.5;
             boolean flag = true;
+            i = 10;
+            f = i + f;
+            flag = 
             
     }
 }
